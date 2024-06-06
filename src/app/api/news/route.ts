a3a093a2ecdce1e1bbb0a2aeb1d10996507a1069
Guilherme Rodrigues <guilhermelindinho2@hotmail.com>
2024-06-06 20:41:28 -0300

    criar o método GET filtrado da API e criar a key da api NewsApi nas váriaveis de ambiente

---
+++ src/app/api/news/route.ts
@@ -0,0 +1,63 @@
+import { NextRequest, NextResponse } from "next/server";
+import { NewsApiOk } from "../interfaces/news-api-ok";
+import { NewsApiError } from "../interfaces/news-api-error";
+
+enum StatusCode {
+    BAD_REQUEST = 400
+}
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams
+
+    const sortBy = searchParams.get("sortBy") ? searchParams.get("sortBy") : "publishedAt"
+    const page = searchParams.get("page") ? parseInt(searchParams.get("page") as string) : 1
+    const items = searchParams.get("items") ? parseInt(searchParams.get("items") as string) : 10
+    const language = searchParams.get("language") ? searchParams.get("language") : "pt"
+
+    if(!items || items < 1) {
+        return NextResponse.json({
+            status: StatusCode.BAD_REQUEST,
+            message: "Query params is incorrect."
+        }, {
+            status: StatusCode.BAD_REQUEST
+        })
+    }
+
+    const apiKey = process.env.NEWSAPI_KEY
+
+    const response = await fetch(
+        `https://newsapi.org/v2/everything?q=a&sortBy=${sortBy}&pageSize=${items}&page=${page}&language=${language}&apiKey=${apiKey}`
+    )
+
+    if(response.ok) {
+        const data: NewsApiOk = await response.json()
+
+        const filteredData = {
+            results: data.totalResults,
+            maxPages: (data.totalResults / items) > 10 ? Math.floor(100 / items) : Math.floor(data.totalResults / items), // Esse "maxPages" é só para no front, eu ter uma ideia de quantas paginas irão ter
+            articles: data.articles.map(article => ({
+                font: {
+                    name: article.source.name,
+                    url: article.url
+                },
+
+                publishedAt: article.publishedAt,
+                imageUrl: article.urlToImage,
+                title: article.title,
+                introduction: article.description,
+                content: article.content
+            }))
+        }
+
+        return NextResponse.json(filteredData)
+    }else {
+        const data: NewsApiError = await response.json()
+
+        return NextResponse.json({
+            status: response.status,
+            message: data.message
+        }, {
+            status: response.status
+        })
+    }
+}
